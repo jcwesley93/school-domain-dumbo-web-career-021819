5123ab38c9c0c6a29507abcdba28b42a483bcfb7
Jaela Wesley <jcwesley93@gmail.com>
2019-02-22 16:43:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+
+require 'pry'
+class School
+  
+attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster.key?(grade)
+        @roster[grade] << name
+   else 
+     @roster[grade] = []
+        @roster[grade] << name
+  end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+   
+  end 
+  
+  def sort 
+#the tests are asking for the keys to be sorted, and for the values of the array (the value to the key), to be sorted as well. ???
+
+    @roster.sort_by { |grade, name| grade }
+      grade.sort_by { |name| name }
+    # @roster.each do |grade, names|
+    #   names.sort
+    end
+  end 
+  
+  
